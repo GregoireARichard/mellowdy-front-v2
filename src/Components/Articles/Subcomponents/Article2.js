b533e6bf30c9de79d84051c7614904b728c51a7c
LouisCuenot <70881127+LouisCuenot@users.noreply.github.com>
2022-02-18 12:29:52 +0100

    Add articles content

--- src/Components/Articles/Subcomponents/Article2.js
+++ src/Components/Articles/Subcomponents/Article2.js
@@ -1,7 +1,24 @@
 function Article2() {
     return (
       <div className="article">
-          sossur2
+          <h1>Technical part</h1>
+          <p>
+            Mellowdy is a relatively simple site, as you can see on 
+            our github <a href="https://github.com/kokorirko/mellowdy-front-v2">front</a> and 
+            <a href="https://github.com/kokorirko/mellowdy"> back</a> for that we used 
+            several different technologies.<br/><br/>
+            For the front end we used React.js and Axios for the API. 
+            At the base the web application was written with 
+            Typescript but the latter posed problems with libraries 
+            that we used. In this case it was fseehawer/circular-clider 
+            and mui. For the main page we also use Three.js with React-Three-Fiber 
+            to generate 
+            the 3D components.<br/><br/>
+            For the back end and server part, we use symfony 
+            (which is a PHP framework) and MongoDB. As I said before, 
+            the link between the back and the front is made by an API 
+            called Axios, which sends requests to transmit information. ⚙️
+          </p>
       </div>
     );
   }
--- src/Components/Articles/Subcomponents/Article6.js
+++ src/Components/Articles/Subcomponents/Article6.js
@@ -1,7 +1,33 @@
 function Article6() {
     return (
       <div className="article">
-          sossur6
+          <h1>What knowledges Mellowdy brought us (Part 3)</h1>
+          <p>
+            This is kind of a heavy one. We like to think we represent 
+            transparency. Delving into these digital mines and coming up 
+            with jewels of good or bad and horrible. We took a good look 
+            and imagined this world for what it was, crawling behind 
+            boardroom doors, crowd sourcing salary content, 
+            videoing acts of aggression and immorality and sharing them 
+            in this country, the world. <br/>We shine lights at black corners, 
+            finding out all those things that permit greed and immorality 
+            and persecution to survive and fester and thrive. We ended 
+            thinking that years of education is equivalent to merit, 
+            we stopped buying into these sales pitches, yet things 
+            thought to make us look like we were all winners. And we 
+            just tried to make something out of it. Chose a lighthearted 
+            design, connected with a company which seems to uphold 
+            benevolent values (Spotify) and just made something that 
+            started out as a tiny idea in our young minds.<br/> We didn't 
+            think of the marketing value, nor the money, nor the 
+            future ad plan. Nah, we decided to make something simple, 
+            fun and attainable for everyone (pretty basic human 
+            functions if you ask me, but sadly rarer and rarer). 
+            From a few students out in Paris, we hope you really like our 
+            project and take the time to share it with people you enjoy 
+            connecting with. 'Cause connections are kind of all we have 
+            left. 💞
+          </p>
       </div>
     );
   }
